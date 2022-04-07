a1627e9d067562fcfb327bae0341f5623d7e05a2
Abhay Tomer <69842633+Herobrine25mcpe@users.noreply.github.com>
2022-04-07 22:40:52 +0530

    Add files via upload

---
+++ colorizer_model.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  6 19:23:41 2022
+
+@author: Abhay
+"""
+
+from keras.layers import Conv2D, UpSampling2D, Input,Dense,MaxPooling2D
+from keras.models import Sequential
+
+
+model = Sequential()
+
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2, input_shape=(256, 256, 1)))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3,3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(256, (3,3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3,3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
+model.add(Conv2D(512, (3,3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3,3), activation='relu', padding='same'))
+
+#Decoder
+#Decoder
+#Note: For the last layer we use tanh instead of Relu.
+#This is because we are colorizing the image in this layer using 2 filters, A and B.
+#A and B values range between -1 and 1 so tanh (or hyperbolic tangent) is used
+#as it also has the range between -1 and 1.
+#Other functions go from 0 to 1.
+model.add(Conv2D(128, (3,3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(64, (3,3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(32, (3,3), activation='relu', padding='same'))
+model.add(Conv2D(16, (3,3), activation='relu', padding='same'))
+model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.compile(optimizer='adam', loss='mse' , metrics=['accuracy'])
+model.summary()
